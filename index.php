e29babacabb58fcc4d48e94073dbb01b74309d89
titekk <deltite4@gmail.com>
2026-06-11 08:44:35 -0300

    Adicionado o menu no projeto e algumas modificações no index

--- index.php
+++ index.php
@@ -1,30 +1,10 @@
 <?php
-//OBSERVAÇÃO: 
-// include: Emite um aviso (Warning) e continua a execução do script.
-// require: Emite um erro fatal (Fatal Error) e interrompe a execução do script
+include_once 'modulos/topo.php';
 
-//o uso de once Verifica se o arquivo já foi incluído ou carregado. Se sim, não o inclui ou carrega novamente
+include_once 'modulos/menu.php';
 
-// Inclui o topo da página (Tags HTML, Head)
-require_once 'modulos/topo.php';
-
-// Inclui o menu de navegação
-require_once 'modulos/menu.php';
 ?>
 
-<main class="container">
-    <div class="row">
-        <?php 
-        // Inclui a seção de conteúdo principal
-        require_once 'modulos/conteudo.php'; 
-        
-        // Inclui a barra lateral
-        require_once 'modulos/lateral.php'; 
-        ?>
-    </div>
-</main>
-
 <?php
-// Inclui o rodapé (Fechamento do HTML)
 require_once 'modulos/rodape.php';
 ?>
